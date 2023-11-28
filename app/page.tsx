3b568c6c5ab10693f8fa68dae3b07ea9f410a553
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-28 16:14:51 +0000

    Add login page UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image'
+import loginIllustration from '@/app/ui/assets/login-illutration.svg'
+import LoginButton from '@/app/ui/login/button'
+import styles from '@/app/ui/login/login.module.css'
+
 export default function LoginPage() {
   return (
-    <p>Login page</p>
+    <div className={styles.loginContainer}>
+      <Image 
+        src={loginIllustration}
+        width={250}
+        height={250}
+        alt="Um homem sentado em uma cadeira comendo uma pizza."
+      />
+      <div>
+        <h1>Desafie a si mesmo</h1>
+        <p>Comece uma dieta balanceada e saudável</p>
+      </div>
+      <LoginButton />
+    </div>
   )
 }
