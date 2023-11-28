af714e58aff2466e7283decf49e81a827d768255
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-28 16:50:26 +0000

    Refactor login page layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import styles from '@/app/ui/login/login.module.css'
 export default function LoginPage() {
   return (
     <div className={styles.loginContainer}>
-      <Image 
-        src={loginIllustration}
-        width={250}
-        height={250}
-        alt="Um homem sentado em uma cadeira comendo uma pizza."
-      />
+      <div className={styles.illustrationContainer}>
+        <Image
+          src={loginIllustration}
+          width={250}
+          height={250}
+          alt="Um homem sentado em uma cadeira comendo uma pizza."
+        />
+      </div>
       <div>
-        <h1>Desafie a si mesmo</h1>
-        <p>Comece uma dieta balanceada e saudável</p>
+        <h1 className={styles.slogan}>Desafie a si mesmo</h1>
+        <p className={styles.sloganAction}>Comece uma dieta balanceada e saudável</p>
       </div>
       <LoginButton />
     </div>
