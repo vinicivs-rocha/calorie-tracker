ab757bcd85c144fbc34897d8e58e34293409f045
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-28 16:50:36 +0000

    Update login button styles

--- app/ui/login/button.tsx
+++ app/ui/login/button.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 import loginIcon from "@/app/ui/assets/login-icon.svg";
+import styles from '@/app/ui/login/login.module.css';
 
 export default function LoginButton() {
   return (
-    <button>
+    <button className={styles.button}>
       <Image
         src={loginIcon}
         alt={""}
         width={24}
         height={24}
       />
-      <span>Entrar com o Google</span>
+      <span className={styles.buttonText}>Entrar com o Google</span>
     </button>
   )
 }
