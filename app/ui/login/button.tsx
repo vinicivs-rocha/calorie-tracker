7e76e6c5011222719e400ffde5052300cf027aa7
Vinícius Rocha <viniciusroch65@gmail.com>
2023-11-28 21:43:11 -0300

    Add Google sign-in functionality to LoginButton component

--- app/ui/login/button.tsx
+++ app/ui/login/button.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import Image from 'next/image';
 import loginIcon from "@/app/ui/assets/login-icon.svg";
 import styles from '@/app/ui/login/login.module.css';
+import { signIn } from 'next-auth/react';
 
 export default function LoginButton() {
+  const handleClick = async () => {
+    signIn('google')
+  };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={handleClick}>
       <Image
         src={loginIcon}
         alt={""}
