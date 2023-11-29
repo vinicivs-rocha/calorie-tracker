713ba50eeaaf9745831df6637be521ebc606ef4e
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-29 16:37:10 +0000

    Update header component to include Navigation component

--- app/ui/home/header.tsx
+++ app/ui/home/header.tsx
@@ -1,12 +1,14 @@
 import UserInfo from "@/app/ui/home/user-info";
 import styles from '@/app/ui/home/home.module.css';
-import CaloriesInfo from "./calories-info";
+import CaloriesInfo from "@/app/ui/home/calories-info";
+import Navigation from "@/app/ui/home/navigation";
 
 export default function HomeHeader() {
   return (
     <header className={styles.headerContainer}>
       <UserInfo />
       <CaloriesInfo />
+      <Navigation />
     </header>
   )
 }
