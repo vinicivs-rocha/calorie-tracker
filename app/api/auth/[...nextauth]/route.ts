a51a573373c581d9e91371764664b183dea40d67
Vinícius Rocha <viniciusroch65@gmail.com>
2023-11-28 21:43:15 -0300

    Add Google authentication provider to NextAuth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      name: "Google",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '/',
+  },  
+})
+
+export { handler as GET, handler as POST }
