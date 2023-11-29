606ad12d2dbadca88b15c7d675504d99018c3cca
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-29 16:08:21 +0000

    Add CaloriesInfo component to HomeHeader

--- app/ui/home/header.tsx
+++ app/ui/home/header.tsx
@@ -1,10 +1,12 @@
 import UserInfo from "@/app/ui/home/user-info";
 import styles from '@/app/ui/home/home.module.css';
+import CaloriesInfo from "./calories-info";
 
 export default function HomeHeader() {
   return (
     <header className={styles.headerContainer}>
-      <UserInfo/>
+      <UserInfo />
+      <CaloriesInfo />
     </header>
   )
 }
