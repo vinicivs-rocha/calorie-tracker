38c38290c8ed80021c719fe38c2e68b0e1e46e0c
Vinicius Rocha <viniciusroch65@gmail.com>
2023-11-30 15:08:46 +0000

    Fix import paths and handle undefined tab value in Home component

--- app/(protected-segment)/home/layout.tsx
+++ app/(protected-segment)/home/layout.tsx
@@ -1,4 +1,4 @@
-import HomeHeader from "@/app/ui/home/header";
+import HomeHeader from "@/app/ui/home/header/header";
 
 export default function HomeLayout({children}: { children: React.ReactNode}) {
   return (
--- app/(protected-segment)/home/page.tsx
+++ app/(protected-segment)/home/page.tsx
@@ -2,10 +2,10 @@ import CurrentMeals from "@/app/ui/home/current/current";
 import MealsHistory from "@/app/ui/home/history/history";
 import styles from '@/app/ui/home/home.module.css';
 
-export default function Home({searchParams}: { params: {}, searchParams: { tab: 'current' | 'history' } }) {
+export default function Home({searchParams}: { params: {}, searchParams: { tab: ('current' | 'history') | undefined  } }) {
   return (
     <main className={styles.mainContainer}>
-    { searchParams.tab === 'current' ? <CurrentMeals /> : <MealsHistory />}
+    { (searchParams.tab === 'current' || !searchParams.tab) ? <CurrentMeals /> : <MealsHistory />}
     </main>
   )
 }
