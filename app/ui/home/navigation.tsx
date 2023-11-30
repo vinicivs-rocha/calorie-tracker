fac0caff5544ac8682a7460a06ccf9f6ecb48ca2
CVinicius Rocha <viniciusroch65@gmail.com>
2023-11-30 12:17:30 +0000

    Add tab navigation functionality to the home page

--- app/ui/home/navigation.tsx
+++ app/ui/home/navigation.tsx
@@ -3,13 +3,17 @@
 import styles from "@/app/ui/home/home.module.css";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import clsx from "clsx";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export default function Navigation() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const activeTab = searchParams.get("tab") ?? "current";
+  
+  const navigationContainer = useRef<HTMLUListElement>(null);
+  const navigationTabs = useRef<HTMLLIElement[]>([]);
+  const activeTabIndicator = useRef<HTMLDivElement>(null);
 
   const createQueryString = useCallback((tab: string) => {
     const params = new URLSearchParams(searchParams);
@@ -17,19 +21,47 @@ export default function Navigation() {
 
     return params.toString();
   }, [searchParams])
+
   function navigateTo(tab: string) {
     const queryString = createQueryString(tab);
     router.push(`${pathname}?${queryString}`);
   }
 
+  function calculateIndicatorLeft(tab: HTMLLIElement) {
+    const tabLeft = tab.getBoundingClientRect().left;
+    const containerLeft = navigationContainer.current!.getBoundingClientRect().left;
+    return tabLeft - containerLeft;
+  }
+
+  function slideIndicatorToTab(tab: HTMLLIElement) {
+    const indicatorLeft = calculateIndicatorLeft(tab);
+    const tabWidth = tab.clientWidth;
+
+    activeTabIndicator.current!.style.left = `${indicatorLeft}px`;
+    activeTabIndicator.current!.style.width = `${tabWidth}px`;
+  }
+
+  function activateTab(tab: HTMLLIElement, tabName: string) {
+    navigateTo(tabName);
+    slideIndicatorToTab(tab);
+  }
+
+  useEffect(() => {
+    const currentTabElement = navigationTabs.current[activeTab === "current" ? 0 : 1]
+    slideIndicatorToTab(currentTabElement);
+  }, [])
+ 
   return (
     <nav>
-      <ul className={styles.navigationContainer}>
+      <ul className={styles.navigationContainer} ref={navigationContainer}>
         <li
-          className={clsx({
-            [styles.activeTab]: activeTab === "current",
-          })}
-          onClick={() => navigateTo("current")}
+          className={clsx(
+            {
+              [styles.activeTab]: activeTab === "current",
+            }
+          )}
+          onClick={({currentTarget}) => activateTab(currentTarget, "current")}
+          ref={el => navigationTabs.current.push(el!)}
         >
           Atual
         </li>
@@ -37,10 +69,12 @@ export default function Navigation() {
           className={clsx({
             [styles.activeTab]: activeTab === "history",
           })}
-          onClick={() => navigateTo("history")}
+          onClick={({currentTarget}) => activateTab(currentTarget, "history")}
+          ref={el => navigationTabs.current.push(el!)}
         >
           Histórico
         </li>
+        <div className={styles.activeTabIndicator} ref={activeTabIndicator}></div>
       </ul>
     </nav>
   );
