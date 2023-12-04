f99113fa41a45c2166f16e26a1a5f7378ec45158
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-04 14:56:47 +0000

    Add numberWithCommas function to format caloriesGoal

--- app/(protected-segment)/home/(header)/calories-info.tsx
+++ app/(protected-segment)/home/(header)/calories-info.tsx
@@ -1,16 +1,23 @@
 import styles from "@/app/(protected-segment)/home/(header)/header.module.css";
 import ProgressBar from "@/app/(protected-segment)/home/(header)/progress-bar";
+import CaloriesGoalNumber from './goal-number';
 
 export default function CaloriesInfo() {
   const caloriesGoal = 2000;
   const caloriesConsumed = 1500;
   const currentIntakePercentage = (caloriesConsumed / caloriesGoal) * 100;
 
+  function numberWithCommas(x: number) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  }
+
   return (
     <div className={styles.calorieInfoContainer}>
       <div className={styles.calorieData}>
         <span className={styles.goalText}>Sua meta de calorias diárias é:</span>
-        <span className={styles.goalNumber}>{caloriesGoal}</span>
+        <CaloriesGoalNumber>
+          {numberWithCommas(caloriesGoal)}
+        </CaloriesGoalNumber>
       </div>
       <div>
         <ProgressBar progress={currentIntakePercentage} />
