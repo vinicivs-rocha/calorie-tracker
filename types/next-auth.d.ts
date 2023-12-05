1756ffafee6fc8d7e07ebdad32b1241adfa08bce
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-05 12:03:41 +0000

    Update next-auth.d.ts to include DefaultSession

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth, {DefaultSession} from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
       uid: string
-    }
+    } & DefaultSession["user"]
   }
 }
