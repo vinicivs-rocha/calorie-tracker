9643b1141d9178e43410cbc7177e28e49405859c
Vinícius Rocha <viniciusroch65@gmail.com>
2023-12-05 18:26:05 -0300

    Refactor current meals component and add server session and meals retrieval

--- app/(protected-segment)/home/current.tsx
+++ app/(protected-segment)/home/current.tsx
@@ -2,13 +2,14 @@ import MealCard from "./(current)/meal-card";
 import StartDayButton from "./(current)/start-button";
 import styles from "./(current)/current.module.css";
 import CurrentMealsFooter from "./(current)/footer";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getMeals } from "@/lib/data";
 
-export default function CurrentMeals() {
-  const meals = [
-    { name: "Café da manhã", id: 1 },
-    { name: "Almoço", id: 2 },
-    { name: "Jantar", id: 3 },
-  ];
+export default async function CurrentMeals() {
+  const { user } = (await getServerSession(authOptions))!;
+  const meals = await getMeals(user.uid);
+
   return (
     <>
       <div className={styles.currentMealsContainer}>
