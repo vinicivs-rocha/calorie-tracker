e1257b546fc0fe6b717f0bd904f62e02a67ad437
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-05 15:46:33 +0000

    Refactor data.ts imports and add getCaloriesIntake

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,19 @@
-import goalsCollection from "@/utils/collections/goals";
+import { feedingsCollection, goalsCollection } from '@/utils/collections/';
 
 export async function getCalorieGoal(userUid: string): Promise<number> {
   const userDailyConsumptionGoals = await goalsCollection().doc(userUid).get();
   if (!userDailyConsumptionGoals.exists || !userDailyConsumptionGoals.data())
     return 0;
   const { calories } = userDailyConsumptionGoals.data()!;
-  
+
   return calories;
+}
+
+export async function getCaloriesIntake(userUid: string): Promise<number> {
+  const lastFeeding = await feedingsCollection(userUid)
+    .orderBy('createdAt', 'desc')
+    .limit(1)
+    .get();
+  const { intake } = lastFeeding.docs[0].data();
+  return intake;
 }
