83543d07a5724741769e721148d99737eae410fd
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-05 12:00:48 +0000

    Update NextAuth configuration to add user id to session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,35 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      name: "Google",
+      name: 'Google',
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
-    })
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
   ],
   pages: {
     signIn: '/',
-  },  
-}
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.uid = token.sub ?? '';
+        return session;
+      }
+      return session;
+    },
+  },
+};
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
