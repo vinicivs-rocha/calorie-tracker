c66c4930a3a8d62df2e5c50d159a24dad39cf14f
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-05 14:06:47 +0000

    Update Firestore import in firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,9 +1,10 @@
 import { credential, initializeApp, ServiceAccount } from "firebase-admin";
-import { getFirestore } from "firebase-admin/firestore";
+import { FirestoreDataConverter, getFirestore } from "firebase-admin/firestore";
 import serviceAccount from '../serviceAccountKey.json';
 
 initializeApp({
   credential: credential.cert(serviceAccount as ServiceAccount),
 })
 
-export const db = getFirestore();
+
+export const firestore = getFirestore();
