ec9243b8b131b49092cd77decbab130b3e00c50a
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-06 12:40:09 +0000

    Add window context and provider

---
+++ app/(protected-segment)/home/(header)/window-context.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { createContext } from 'react';
+
+export const WindowWidthContext = createContext<number>(0);
---
+++ app/(protected-segment)/home/(header)/window-provider.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { WindowWidthContext } from './window-context';
+
+export default function WindowWidthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [viewportWidth, setViewportWidth] = useState(0);
+
+  useEffect(() => setViewportWidth(window.innerWidth), []);
+
+  return (
+    <WindowWidthContext.Provider value={viewportWidth}>
+      {children}
+    </WindowWidthContext.Provider>
+  );
+}
