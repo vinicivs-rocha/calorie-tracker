c9430051b60c17464529935df35a2b0b1032f837
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-06 13:59:06 +0000

    Refactor header macros component

--- app/(protected-segment)/home/(header)/macros.tsx
+++ app/(protected-segment)/home/(header)/macros.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+'use client';
+
+import clsx from 'clsx';
+import React, { useContext } from 'react';
+import styles from './header.module.css';
+import { WindowWidthContext } from './window-context';
+import MacroQuantity from './macro-quantity';
 
 export default function MacrosData() {
+  const viewportWidth = useContext(WindowWidthContext);
+  const macros = {
+    carbo: 35,
+    protein: 27,
+    fat: 21,
+  };
+
   return (
-    <div>MacrosData</div>
-  )
+    <div
+      className={clsx({
+        [styles.hidden]: viewportWidth < 1400,
+        [styles.macrosData]: true,
+      })}
+    >
+      {Object.entries(macros).map(([key, value]) => (
+        <MacroQuantity key={key} name={key} quantity={value} />
+      ))}
+    </div>
+  );
 }
