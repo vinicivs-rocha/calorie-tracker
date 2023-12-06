7bb65bef8ddd29398495dfb3903a1a2fa98a7109
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-06 13:59:16 +0000

    Add macro quantity component to header module

---
+++ app/(protected-segment)/home/(header)/macro-quantity.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './header.module.css'
+
+export default function MacroQuantity({ name, quantity }: { name: string, quantity: number }) {
+  function formatName(name: string) {
+    switch (name) {
+      case 'carbo':
+        return 'Carboidratos'
+      case 'protein':
+        return 'Proteínas'
+      case 'fat':
+        return 'Gorduras'
+      default:
+        break;
+    }
+  }
+  return (
+    <div className={styles.macrosItem}>
+      <span>{formatName(name)}</span>
+      <div className={styles.macrosItemSeparator}/>
+      <span>{quantity}g</span>
+    </div>
+  )
+}
