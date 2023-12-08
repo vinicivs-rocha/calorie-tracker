ef05c510e47dafb62d1c2e9313c445fb38fa583e
Vinicius Rocha <viniciusroch65@gmail.com>
2023-12-08 13:39:24 +0000

    Refactor import statements and update meal rendering logic

--- app/(protected-segment)/home/current.tsx
+++ app/(protected-segment)/home/current.tsx
@@ -1,23 +1,22 @@
-import MealCard from "./(current)/meal-card";
+import MealCard from './(current)/meal-card';
 import TopButton from './(current)/top';
-import styles from "./(current)/current.module.css";
-import CurrentMealsFooter from "./(current)/footer";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getMeals } from "@/lib/data";
+import styles from './(current)/current.module.css';
+import CurrentMealsFooter from './(current)/footer';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getMeals } from '@/lib/data';
 
 export default async function CurrentMeals() {
-  const { user } = (await getServerSession(authOptions))!;
-  const meals = await getMeals(user.uid);
+  const meals = await getMeals();
 
   return (
     <>
       <div className={styles.currentMealsContainer}>
         <TopButton />
         <div className={styles.mealCardsContainer}>
-          {meals.map((meal) => (
-            <MealCard key={meal.name} meal={meal} />
-          ))}
+          {meals.map((meal) =>
+            meal ? <MealCard key={meal.name} meal={meal} /> : <></>
+          )}
         </div>
       </div>
       <CurrentMealsFooter />
