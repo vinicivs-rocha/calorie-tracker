2a11f3fbd8b91fb9f204d5916849ed44bd7c835b
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-02 13:36:30 +0000

    🎨 refactor: move component down the tree

--- app/(protected-segment)/home/current.tsx
+++ app/(protected-segment)/home/current.tsx
@@ -1,23 +1,14 @@
-import MealCard from './(current)/meal-card';
 import TopButton from './(current)/top';
 import styles from './(current)/current.module.css';
 import CurrentMealsFooter from './(current)/footer';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { getMeals } from '@/lib/data';
+import MealCardsContainer from './(current)/meals-container';
 
 export default async function CurrentMeals() {
-  const meals = await getMeals();
-
   return (
     <>
       <div className={styles.currentMealsContainer}>
         <TopButton />
-        <div className={styles.mealCardsContainer}>
-          {meals.map((meal) =>
-            meal ? <MealCard key={meal.name} meal={meal} /> : <></>
-          )}
-        </div>
+        <MealCardsContainer />
       </div>
       <CurrentMealsFooter />
     </>
