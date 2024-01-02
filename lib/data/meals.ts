a14e8741dec0de4c7572147921acabb5d643e120
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-02 13:35:36 +0000

    🥅 fix: not found error handling

--- lib/data/meals.ts
+++ lib/data/meals.ts
@@ -3,17 +3,19 @@ import { getDocData } from '@/utils';
 import { getLastDocument } from '@/utils';
 import { feedings, meals } from '@/utils/collections';
 import { getAllCollectionDocs } from '@/utils';
+import { Meal } from '@/types/documents';
+import { ErrorMessage } from '@/types/error';
 
-export async function getMeals() {
+export async function getMeals(): Promise<[Meal[], ErrorMessage]> {
   const userUid = await getUserId();
   const feeding = await getLastDocument(feedings(userUid)) ?? { exists: false };
 
   if (!feeding.exists) {
-    return [];
+    return [[], 'No feedings found'];
   }
 
   const mealsDocs = await getAllCollectionDocs(meals(userUid, feeding.id));
-  const mealsData = mealsDocs.map(getDocData);
+  const mealsData = (await Promise.all(mealsDocs.map(getDocData))).filter(meal => meal !== undefined) as Meal[];
 
-  return Promise.all(mealsData);
+  return [mealsData, null];
 }
