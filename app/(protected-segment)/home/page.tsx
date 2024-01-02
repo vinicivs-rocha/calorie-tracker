2151c253d98fbde761e1fa74cf840d6bfd9b3921
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-02 13:36:54 +0000

    🎨 style: format file

--- app/(protected-segment)/home/page.tsx
+++ app/(protected-segment)/home/page.tsx
@@ -1,11 +1,20 @@
-import CurrentMeals from "@/app/(protected-segment)/home/current";
-import MealsHistory from "@/app/(protected-segment)/home/history";
+import CurrentMeals from './current';
+import MealsHistory from '@/app/(protected-segment)/home/history';
 import styles from '@/app/(protected-segment)/home/home.module.css';
 
-export default function Home({searchParams}: { params: {}, searchParams: { tab: ('current' | 'history') | undefined  } }) {
+export default function Home({
+  searchParams,
+}: {
+  params: {};
+  searchParams: { tab: ('current' | 'history') | undefined };
+}) {
   return (
     <main className={styles.mainContainer}>
-    { (searchParams.tab === 'current' || !searchParams.tab) ? <CurrentMeals /> : <MealsHistory />}
+      {searchParams.tab === 'current' || !searchParams.tab ? (
+        <CurrentMeals />
+      ) : (
+        <MealsHistory />
+      )}
     </main>
-  )
+  );
 }
