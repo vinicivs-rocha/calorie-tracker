5f1b960be1e2cf6476035275a460c739419672b9
Vinícius Rocha <viniciusroch65@gmail.com>
2024-01-04 19:47:47 -0300

    ✨ feat: add navigation to meal adding page

--- app/(protected-segment)/home/(current)/top.tsx
+++ app/(protected-segment)/home/(current)/top.tsx
@@ -1,10 +1,11 @@
-'use client'
+'use client';
 
 import Image from 'next/image';
 import plusSign from '@/app/ui/assets/plus-sign.svg';
 import styles from '@/app/(protected-segment)/home/(current)/current.module.css';
 import { poppins } from '../../../fonts';
 import { initNewFeeding } from '@/lib/actions';
+import Link from 'next/link';
 
 export default function TopButton() {
   return (
@@ -13,10 +14,13 @@ export default function TopButton() {
         <Image src={plusSign} alt='' width={12} height={12} />
         <span className={poppins.className}>Iniciar novo dia</span>
       </button>
-      <button className={`${styles.topButton} ${styles.addMealTopButton}`}>
+      <Link
+        href='/newMeal'
+        className={`${styles.topButton} ${styles.addMealTopButton}`}
+      >
         <Image src={plusSign} alt='' width={12} height={12} />
         <span className={poppins.className}>Adicionar refeição</span>
-      </button>
+      </Link>
     </div>
   );
 }
