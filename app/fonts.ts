b1839b3a4577728ddefe56581991aca4078b42fe
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-05 11:12:38 -0300

    ➕ chore: add shadcn ui package

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,5 +2,6 @@ import { Poppins } from 'next/font/google';
 
 export const poppins = Poppins({
   weight: ['700', '600', '400'],
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: "--font-sans"
 })
