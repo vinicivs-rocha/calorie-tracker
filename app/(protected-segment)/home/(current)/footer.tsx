722fc45c34469b17ea6febc28496769749d69ba4
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-12 13:09:11 -0300

    ✨ feat: add mobile navigation to new meal

--- app/(protected-segment)/home/(current)/footer.tsx
+++ app/(protected-segment)/home/(current)/footer.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import plusSign from '@/app/ui/assets/plus-sign.svg';
 import styles from "./current.module.css";
+import Link from "next/link";
 
 export default function CurrentMealsFooter() {
   return (
     <footer className={styles.footerContainer}>
-      <button className={styles.footerButton}>
+      <Link href='/new-meal' className={styles.footerButton}>
         <Image src={plusSign} alt="" width={16} height={16} />
-      </button>
+      </Link>
     </footer>
   );
 }
