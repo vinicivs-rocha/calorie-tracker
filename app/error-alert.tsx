da0d54fbed897bec1f56401e60ea0cb90704b196
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-12 13:12:07 -0300

    💄 feat: error alert component

---
+++ app/error-alert.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '@/app/ui/components/ui/alert';
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+
+export default function ErrorAlert({
+  errors,
+  cause,
+}: {
+  errors: string[];
+  cause: string;
+}) {
+  if (errors.length === 0) return null;
+
+  return (
+    <AnimatePresence>
+      {errors.map((message) => (
+        <motion.div
+          key={message}
+          initial={{ x: 1000, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -1000, opacity: 0 }}
+        >
+          <Alert className='bg-red-300' variant='destructive'>
+            <ExclamationTriangleIcon className='h-3 w-3' color='black' />
+            <AlertTitle className='text-sm text-slate-700'>
+              Erro ao {cause}
+            </AlertTitle>
+            <AlertDescription className='text-xs text-slate-700'>
+              {message}
+            </AlertDescription>
+          </Alert>
+        </motion.div>
+      ))}
+    </AnimatePresence>
+  );
+}
