f4a6260d035c917e87b563d117d3b11cd39b0b3f
Vinícius Rocha <viniciusroch65@gmail.com>
2024-01-14 17:56:32 -0300

    🎨 refactor: isolate getMeals responsabilities

--- lib/data/meals.ts
+++ lib/data/meals.ts
@@ -6,16 +6,23 @@ import { getAllCollectionDocs } from '@/lib/utils';
 import { Meal } from '@/types/documents';
 import { ErrorMessage } from '@/types/error';
 
-export async function getMeals(): Promise<[Meal[], ErrorMessage]> {
+export async function getMealData(mealDoc: FirebaseFirestore.QueryDocumentSnapshot<Meal, FirebaseFirestore.DocumentData>): Promise<Meal> {
+  const mealData = (await getDocData(mealDoc)) as Meal;
+
+  return mealData;
+}
+
+export async function getMealsSnapshots(): Promise<[FirebaseFirestore.QueryDocumentSnapshot<Meal, FirebaseFirestore.DocumentData>[], ErrorMessage]> {
   const userUid = await getUserId();
-  const feeding = await getLastDocument(feedings(userUid)) ?? { exists: false };
+  const feeding = (await getLastDocument(feedings(userUid))) ?? {
+    exists: false,
+  };
 
   if (!feeding.exists) {
     return [[], 'No feedings found'];
   }
 
   const mealsDocs = await getAllCollectionDocs(meals(userUid, feeding.id));
-  const mealsData = (await Promise.all(mealsDocs.map(getDocData))).filter(meal => meal !== undefined) as Meal[];
-
-  return [mealsData, null];
+  
+  return [mealsDocs, null];
 }
