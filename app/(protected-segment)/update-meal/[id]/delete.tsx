374da7c2cda627590d9e9c934c9fc110e354e658
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-22 11:34:23 -0300

    🍱 feat: add trash sign

--- app/(protected-segment)/update-meal/[id]/delete.tsx
+++ app/(protected-segment)/update-meal/[id]/delete.tsx
@@ -1,7 +1,12 @@
 import styles from "./update-meal.module.css";
+import Image from "next/image";
+import trashSign from "@/app/ui/assets/trash-sign.svg"
+
 
 export default function DeleteButton() {
   return (
-    <button className={styles.delete}>DeleteButton</button>
+    <button className={styles.delete}>
+      <Image src={trashSign} alt='' height={30} width={30} />
+    </button>
   )
 }
