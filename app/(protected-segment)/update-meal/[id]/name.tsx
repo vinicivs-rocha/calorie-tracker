de5f45c0319a48089977d893dfa30ee14d0b3d67
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-24 09:30:13 -0300

    ✨ feat: add meal name state changing

--- app/(protected-segment)/update-meal/[id]/name.tsx
+++ app/(protected-segment)/update-meal/[id]/name.tsx
@@ -1,7 +1,23 @@
-import styles from './update-meal.module.css';
+'use client';
 
-export default function NameInput({ initialValue }: { initialValue: string }) {
+import { useContext } from 'react';
+import styles from './update-meal.module.css';
+import { MealDataContext } from '@/lib/contexts/meal';
+
+export default function NameInput() {
+  const [mealData, setMealData] = useContext(MealDataContext);
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!setMealData) throw Error('setMealData is not defined');
+    if (!event.target) throw Error('event.target is not defined');
+
+    setMealData({
+      ...mealData,
+      name: event.target.value,
+    });
+  }
+
   return (
-    <input type='text' placeholder='Digite o nome da refeição' className={styles.nameInput} value={initialValue}/>
+    <input type='text' placeholder='Digite o nome da refeição' className={styles.nameInput} value={mealData.name} onChange={handleNameChange}/>
   )
 }
