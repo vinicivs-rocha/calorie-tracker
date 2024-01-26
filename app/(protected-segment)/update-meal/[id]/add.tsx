a9add5daa38f5762349f09015903047c307da716
Vinicius Rocha <viniciusroch65@gmail.com>
2024-01-26 11:22:00 -0300

    ✨ feat: add taco food selecting

--- app/(protected-segment)/update-meal/[id]/add.tsx
+++ app/(protected-segment)/update-meal/[id]/add.tsx
@@ -1,11 +1,107 @@
-import styles from "./update-meal.module.css"; 
-import Image from "next/image";
+"use client";
+
 import plusSign from "@/app/ui/assets/plus-sign.svg";
+import { FoodDTO } from "@/types/food";
+import { gql, useQuery } from "@apollo/client";
+import Image from "next/image";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import ConfirmAdding from "./confirm-adding";
+import QuantityInput from "./quantity";
+import styles from "./update-meal.module.css";
+import { MealDataContext } from "@/lib/contexts/meal";
+import TacoItems from "../../taco-items";
 
 export default function AddFoodButton() {
+  const [mealData, setMealData] = useContext(MealDataContext);
+  const activeRef = useRef<HTMLDivElement>(null);
+  const [active, setActive] = useState(false)
+  const [selectedFood, setSelectedFood] = useState<FoodDTO>({})
+  const [isSelectOpen, setIsSelectOpen] = useState(false)
+  
+  function confirmAdding() {
+    setAddedFoods(getFoodData);
+    setActive(false);
+  }
+  
+  function setAddedFoods(setFunction: (prevState: FoodDTO[]) => FoodDTO[]) {
+    if (!setMealData) throw new Error('setMealData is not defined');
+    
+    setMealData((prev) => ({
+      ...prev,
+      foods: setFunction(prev.foods),
+    }));    
+  }
+  const query = gql`
+    query GetFoodById($id: ID!) {
+      getFoodById(id: $id) {
+        id
+        name
+        nutrients {
+          carbohydrates
+          protein
+          lipids
+          kcal
+        }
+      }
+    }
+  `;
+
+  const foodData = useQuery<{ getFoodById: FoodDTO }>(query, {
+    variables: { id: selectedFood.id },
+  });
+
+  const getFoodData = useCallback(
+    (prev: FoodDTO[]) => {
+      if (foodData.loading) {
+        return [];
+      }
+      if (foodData.error) {
+        console.log(foodData.error);
+        return [];
+      }
+      const { getFoodById: food } = foodData.data!;
+      const { totalQuantity, id } = selectedFood;
+      const { name, nutrients } = food;
+      const { carbohydrates, protein, lipids, kcal } = nutrients!;
+      return [
+        ...prev,
+        {
+          id,
+          totalQuantity,
+          name,
+          nutrients: {
+            carbohydrates: (carbohydrates! * totalQuantity!)  / 100,
+            protein: (protein! * totalQuantity!) / 100,
+            lipids: (lipids! * totalQuantity!) / 100,
+            kcal: (kcal! * totalQuantity!) / 100,
+          },
+        },
+      ];
+    },
+    [foodData, selectedFood]
+  );
+
+  useEffect(() => {
+    if (active && activeRef.current) {
+      activeRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [active]);
+
   return (
-    <button className={styles.add}>
-      <Image src={plusSign} alt="" height={16} width={16}/>
-    </button>
+    <>
+      {active ? (
+        <div className={styles.foodInput} ref={activeRef}>
+          <TacoItems setIsSelectOpen={setIsSelectOpen} setSelectedFood={setSelectedFood} />
+          <QuantityInput setSelectedFood={setSelectedFood}/>
+          <div className={styles.buttonsContainer}>
+            <ConfirmAdding onClick={confirmAdding}/>
+          </div>
+        </div>
+      ) : (
+        <button className={styles.add} onClick={() => setActive(true)}>
+          <Image src={plusSign} alt="" height={16} width={16}/>
+        </button>
+      )}
+    </>
   )
 }
--- app/(protected-segment)/update-meal/[id]/foods.tsx
+++ app/(protected-segment)/update-meal/[id]/foods.tsx
@@ -1,5 +1,11 @@
+'use client';
+
 import styles from './update-meal.module.css';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '@/lib/graphql';
 
 export default function Foods({ children }: { children: React.ReactNode }) {
-  return <div className={styles.foods}>{children}</div>;
+  return <div className={styles.foods}>
+    <ApolloProvider client={client}>{children}</ApolloProvider>
+  </div>;
 }
