477b390a730eb8fb59a987ae4d5a9854e0daa92b
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-06 11:05:08 -0300

    💄 feat: add food card animation

--- app/(protected-segment)/update-meal/[id]/food.tsx
+++ app/(protected-segment)/update-meal/[id]/food.tsx
@@ -1,13 +1,18 @@
-import styles from './update-meal.module.css';
+import { motion } from 'framer-motion';
 import Select from './select';
+import styles from './update-meal.module.css';
 
-export default function Food({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className={styles.food}>
-    <Select />
-    {children}
-  </div>;
+// TODO - Add mobile drag animation
+export default function Food({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      className={styles.food}
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.2, type: 'spring', stiffness: 200, damping: 9 }}
+    >
+      <Select />
+      {children}
+    </motion.div>
+  );
 }
