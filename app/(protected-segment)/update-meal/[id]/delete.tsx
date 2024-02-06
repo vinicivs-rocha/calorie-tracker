062c0515aa143a5b97e01fb92fb66036f5b9d99d
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-06 13:03:47 -0300

    💄 feat: add opacity transition
    Add opacity fading based on card dragging

--- app/(protected-segment)/update-meal/[id]/delete.tsx
+++ app/(protected-segment)/update-meal/[id]/delete.tsx
@@ -1,12 +1,16 @@
 import trashSign from '@/app/ui/assets/trash-sign.svg';
+import { DragOpacityContext } from '@/lib/contexts/drag';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useContext } from 'react';
 import styles from './update-meal.module.css';
 
 // TODO - Add context food deletion
 export default function DeleteButton() {
+  const [dragOpacity] = useContext(DragOpacityContext);
+  const opacity = dragOpacity > 1 ? 0 : dragOpacity;
   return (
-    <motion.button initial={{ opacity: 0 }} className={styles.delete}>
+    <motion.button className={`${styles.delete}`} style={{ opacity }}>
       <Image src={trashSign} alt='' height={30} width={30} />
     </motion.button>
   );
--- app/(protected-segment)/update-meal/[id]/food.tsx
+++ app/(protected-segment)/update-meal/[id]/food.tsx
@@ -1,18 +1,53 @@
+'use client';
+
+import { DragOpacityContext } from '@/lib/contexts/drag';
 import { motion } from 'framer-motion';
+import { useContext, useEffect, useRef, useState } from 'react';
+import DeleteButton from './delete';
 import Select from './select';
 import styles from './update-meal.module.css';
 
 // TODO - Add mobile drag animation
 export default function Food({ children }: { children: React.ReactNode }) {
+  const foodRef = useRef<HTMLDivElement>(null);
+  const [foodOffset, setFoodOffset] = useState<number>();
+  const [_, setDragOpacity] = useContext(DragOpacityContext);
+
+  if (setDragOpacity) {
+    setDragOpacity(() => {
+      if (!foodOffset) return 0;
+      return -(foodOffset - 20.5) / 50;
+    });
+  }
+
+  useEffect(() => {
+    setFoodOffset(foodRef.current?.getBoundingClientRect().x);
+  }, [foodRef]);
+
   return (
-    <motion.div
-      className={styles.food}
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.2, type: 'spring', stiffness: 200, damping: 9 }}
-    >
-      <Select />
-      {children}
-    </motion.div>
+    <div className={styles.foodContainer}>
+      <motion.div
+        className={styles.food}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.2,
+          type: 'spring',
+          stiffness: 200,
+          damping: 9,
+        }}
+        drag='x'
+        dragConstraints={{ left: -50, right: 0 }}
+        dragElastic={0}
+        onDrag={() => {
+          setFoodOffset(foodRef.current?.getBoundingClientRect().x);
+        }}
+        ref={foodRef}
+      >
+        <Select />
+        {children}
+      </motion.div>
+      <DeleteButton />
+    </div>
   );
 }
