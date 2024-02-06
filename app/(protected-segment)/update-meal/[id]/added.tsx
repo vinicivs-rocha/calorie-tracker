b44f4697257d612504412b1432692408a69604d3
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-06 11:00:43 -0300

    💄 feat: add rendering to already registered foods

--- app/(protected-segment)/update-meal/[id]/added.tsx
+++ app/(protected-segment)/update-meal/[id]/added.tsx
@@ -1,7 +1,42 @@
+'use client';
+import { MealDataContext } from '@/lib/contexts/meal';
+import { AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import DeleteButton from './delete';
+import Food from './food';
+import Macro from './macro';
+import NutritionalData from './nutritional-data';
 import styles from './update-meal.module.css';
 
-export default function AddedFoods({ children }: { children: React.ReactNode }) {
+export default function AddedFoods() {
+  const [mealData] = useContext(MealDataContext);
   return (
-    <div className={styles.addedFoods}>{children}</div>
-  )
+    <AnimatePresence>
+      {mealData.foods.map(({ name, nutrients }, index) => (
+        <Food key={index}>
+          <div className={styles.foodData}>
+            <h2 className={styles.foodName}>{name}</h2>
+            <NutritionalData>
+              <Macro
+                amount={nutrients?.carbohydrates!}
+                name='Carbos'
+                key={'Carbos'}
+              />
+              <Macro
+                amount={nutrients?.protein!}
+                name='Proteínas'
+                key={'Proteínas'}
+              />
+              <Macro
+                amount={nutrients?.lipids!}
+                name='Gorduras'
+                key={'Gorduras'}
+              />
+            </NutritionalData>
+          </div>
+          <DeleteButton />
+        </Food>
+      ))}
+    </AnimatePresence>
+  );
 }
