644cdb49b6e7277fcda862c5d4888bc27aeffaf2
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-08 09:57:56 -0300

    💄 feat: provide viewport size context to foods

--- app/(protected-segment)/update-meal/[id]/added.tsx
+++ app/(protected-segment)/update-meal/[id]/added.tsx
@@ -2,6 +2,7 @@
 import { MealDataContext } from '@/lib/contexts/meal';
 import { AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
+import WindowWidthProvider from '../../window-provider';
 import DragOpacityContextProvider from './drag-provider';
 import Food from './food';
 import Macro from './macro';
@@ -12,32 +13,34 @@ export default function AddedFoods() {
   const [mealData] = useContext(MealDataContext);
   return (
     <AnimatePresence>
-      {mealData.foods.map(({ name, nutrients }, index) => (
-        <DragOpacityContextProvider initialValue={0} key={index}>
-          <Food>
-            <div className={styles.foodData}>
-              <h2 className={styles.foodName}>{name}</h2>
-              <NutritionalData>
-                <Macro
-                  amount={nutrients?.carbohydrates!}
-                  name='Carbos'
-                  key={'Carbos'}
-                />
-                <Macro
-                  amount={nutrients?.protein!}
-                  name='Proteínas'
-                  key={'Proteínas'}
-                />
-                <Macro
-                  amount={nutrients?.lipids!}
-                  name='Gorduras'
-                  key={'Gorduras'}
-                />
-              </NutritionalData>
-            </div>
-          </Food>
-        </DragOpacityContextProvider>
-      ))}
+      <WindowWidthProvider>
+        {mealData.foods.map(({ name, nutrients }, index) => (
+          <DragOpacityContextProvider initialValue={0} key={index}>
+            <Food index={index}>
+              <div className={styles.foodData}>
+                <h2 className={styles.foodName}>{name}</h2>
+                <NutritionalData>
+                  <Macro
+                    amount={nutrients?.carbohydrates!}
+                    name='Carbos'
+                    key={'Carbos'}
+                  />
+                  <Macro
+                    amount={nutrients?.protein!}
+                    name='Proteínas'
+                    key={'Proteínas'}
+                  />
+                  <Macro
+                    amount={nutrients?.lipids!}
+                    name='Gorduras'
+                    key={'Gorduras'}
+                  />
+                </NutritionalData>
+              </div>
+            </Food>
+          </DragOpacityContextProvider>
+        ))}
+      </WindowWidthProvider>
     </AnimatePresence>
   );
 }
