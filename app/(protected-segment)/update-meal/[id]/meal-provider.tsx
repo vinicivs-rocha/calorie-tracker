6d3b68f51ed6a3d730bdcb5632154d958a53e564
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-08 10:01:17 -0300

    🐛 fix: change meal data context provider foods type

--- app/(protected-segment)/update-meal/[id]/meal-provider.tsx
+++ app/(protected-segment)/update-meal/[id]/meal-provider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { MealDataContext } from '@/lib/contexts/meal'
-import { FoodDTO } from '@/types/food'
-import React, { PropsWithChildren } from 'react'
+import { MealDataContext } from '@/lib/contexts/meal';
+import { FoodDTO } from '@/types/food';
+import React, { PropsWithChildren } from 'react';
 
-export default function MealDataContextProvider({ children, initialValue }: PropsWithChildren<{
-  initialValue: { name: string, foods: FoodDTO[] }
+export default function MealDataContextProvider({
+  children,
+  initialValue,
+}: PropsWithChildren<{
+  initialValue: { name: string; foods: FoodDTO & { selected: boolean }[] };
 }>) {
-  const [mealData, setMealData] = React.useState<{ name: string, foods: FoodDTO[] }>(initialValue)
+  const [mealData, setMealData] = React.useState<{
+    name: string;
+    foods: FoodDTO[];
+  }>(initialValue);
   return (
     <MealDataContext.Provider value={[mealData, setMealData]}>
       {children}
     </MealDataContext.Provider>
-  )
+  );
 }
