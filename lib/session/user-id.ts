ad802b5e849db2fc8758415bf6b65db91e245ce4
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-15 11:07:54 -0300

    🐛 fix: restrict user id retrieval to server

--- lib/session/user-id.ts
+++ lib/session/user-id.ts
@@ -1,6 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { SessionError } from "@/errors/session";
 import { getServerSession } from "next-auth";
+import 'server-only';
 
 export async function getUserId() {
   const session = await getServerSession(authOptions);
