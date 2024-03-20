bf83bda4dcf417f72058ec01ebde7bc503a88566
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-20 10:31:38 -0300

    ✨ feat: provide query client to data fetching components

--- app/(protected-segment)/home/history.tsx
+++ app/(protected-segment)/home/history.tsx
@@ -1,4 +1,6 @@
+import QueryClient from '../query-client';
 import { CalendarContextProvider } from './(history)/calendar-provider';
+import { ConsumedMeals } from './(history)/consumed-meals';
 import DatePicker from './(history)/date-picker';
 import styles from './(history)/history.module.css';
 
@@ -6,7 +8,12 @@ export default function MealsHistory() {
   return (
     <CalendarContextProvider>
       <main className={styles.main}>
-        <DatePicker></DatePicker>
+        <DatePicker />
+        <QueryClient>
+          <div>
+            <ConsumedMeals></ConsumedMeals>
+          </div>
+        </QueryClient>
       </main>
     </CalendarContextProvider>
   );
