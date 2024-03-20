c59e385efbffbe9520d18d592b0d1dca663681b5
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-20 13:58:31 -0300

    💄 feat: add mobile styling

--- app/(protected-segment)/home/history.tsx
+++ app/(protected-segment)/home/history.tsx
@@ -4,13 +4,14 @@ import { ConsumedMeals } from './(history)/consumed-meals';
 import DatePicker from './(history)/date-picker';
 import styles from './(history)/history.module.css';
 
+// TODO - add desktop styling
 export default function MealsHistory() {
   return (
     <CalendarContextProvider>
       <main className={styles.main}>
         <DatePicker />
         <QueryClient>
-          <div>
+          <div className={styles.consumedContainer}>
             <ConsumedMeals></ConsumedMeals>
           </div>
         </QueryClient>
